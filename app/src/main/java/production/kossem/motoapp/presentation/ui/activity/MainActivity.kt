21ad0aefc8d227fc74f00a2afc2a1cfe5a31a849
Kostya <kossem11@gmail.com>
2023-07-18 00:00:49 +0300

    1

---
+++ app/src/main/java/production/kossem/motoapp/presentation/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package production.kossem.motoapp.presentation.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import production.kossem.motoapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/production/kossem/motoapp/presentation/ui/fragmets/FavoritesFragment.kt
@@ -0,0 +1,24 @@
+package production.kossem.motoapp.presentation.ui.fragmets
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import production.kossem.motoapp.R
+
+
+class FavoritesFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+}
